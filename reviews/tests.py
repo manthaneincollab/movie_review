bdae0dc163f928ce9417474df0672a07fa07052c
Michelle Labasbas <manthanein@gmail.com>
2023-04-08 15:52:13 +0000

    created review/tests.py

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Review
 
-# Create your tests here.
+
+class ReviewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.review = Review.objects.create(
+            title="Citizen Kane",
+            director="Orson Welles",
+            actors="Orson Welles and Joseph Cotten",
+            review="One of the greatest films of all time, a must see. A true, timeless masterpiece.",
+            year=1941,
+            stars="ssss",
+        )
